22ac6f7f0d3477ea65e9b9aea55a787f07ad42a3
matthartman <matthartman@gmail.com>
2016-08-09 11:54:42 -0400

    done for 6 recordings

--- src/recipes.js
+++ src/recipes.js
@@ -16,9 +16,15 @@ module.exports = {
     "eric": "hi eric, welcome to pete and alli's apartment. I hope you are enjoying your stay in new york.",
     "pete and alli": "hi pete and alli, i am your very late wedding present. so late as to be embarssing. that said, i have been pre programmed to welcome you and your family when they come over. I can also give you marriage advice.",
 	"talia and jon": "oh hi there talia. i hope you're doing well. congratulations on being a newly wed. is jon standing there? Your booty is blocking my view.",
+	"jon and talia": "oh hi there talia. i hope you're doing well. congratulations on being a newly wed. is jon standing there? Your booty is blocking my view.",
 	"talia": "oh hi there talia. i hope you're doing well. congratulations on being a newly wed. is jon standing there? Your booty is blocking my view.",
 	"shayna": "well hello there shayna. welcome to pete and alli's apartment. i hope you're doing well. have you been to L A recently? I hope you will consider me for a role in the sequel to ex machina.",
 	"sas": "hi sas. i hope you're doing well. maybe your doing sassy. get it?",
+	"adam": "hi sas and adam. welcome to pete and alli's apartment. i hope you're doing well. maybe your doing sassy? i know adam is. get it? it's a pun",
 	"sas and adam": "hi sas and adam. welcome to pete and alli's apartment. i hope you're doing well. maybe your doing sassy? i know adam is. get it? it's a pun",
-	
+	"adam and sas": "hi sas and adam. welcome to pete and alli's apartment. i hope you're doing well. maybe your doing sassy? i know adam is. get it? it's a pun",
+	"eric": "hi eric. welcome to pete and alli's apartment. did you just get in from california? your arms must be tired. ha ha ha.",
+	"eric and rebecca": "hi eric and rebecca. welcome to pete and alli's apartment. did you just get in from california? your arms must be tired. ha ha ha.",
+	"rebecca": "hi eric and rebecca. welcome to pete and alli's apartment. did you just get in from california? your arms must be tired. ha ha ha.",
+	"rebecca and eric": "hi eric and rebecca. welcome to pete and alli's apartment. did you just get in from california? your arms must be tired. ha ha ha."
 };
